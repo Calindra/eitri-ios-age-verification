bfd118d109eb4139e7b61bdfecd6d11f51f771ef
Calindra Build Server <calindra@Calindra-Build-Server.local>
2026-09-17 14:31:11 -0300

    chore: publish eitri-ios-age-verification version 2.0.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "EitriAgeVerification",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         // O produto final inclui tanto o binário quanto as dependências transitivas
